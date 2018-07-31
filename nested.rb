e6488297a5d323e68815e9c50524a96ab02e61a6
Chris Franklin <cf73021@gmail.com>
2018-07-31 14:00:53 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -89,7 +89,7 @@ programmer_hash[:yukihiro_matsumoto] = {
 	:known_for => "Ruby",
 	:languages => ["LISP", "C"]
 }
-programmer_hash[:yukihiro_matsumoto]
+programmer_hash[2]
 end
 
 def changing_alan
