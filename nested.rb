540111ff66f7f25906384375ee1a2a87018414cd
Chris Franklin <cf73021@gmail.com>
2018-07-31 14:02:39 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -85,9 +85,7 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {
-	:known_for => "Ruby",
-	:languages => ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto] = "Test"
 }
 programmer_hash[3]
 end
